5746c6fc190f0f6f32196675e1d25390acedba43
nanilei96 <washingn8028@student.faytechcc.edu>
2024-09-20 00:53:57 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_WashingtonNicollette.py
@@ -0,0 +1,14 @@
+# Nicollette Washington
+# September 20, 2024
+# P1Lab1
+# Using pythons to generate 
+
+
+
+first_name = input('Enter your first name: ')
+
+
+last_name = input('Enter your last name: ')
+
+
+print('Hello,',first_name,last_name,  'Welcome to course 110!')
